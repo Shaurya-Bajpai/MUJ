5c7c9bba3b5013b74683133688e1f748a3426caa
Shaurya Bajpai <shauryabajpai980@gmail.com>
2026-09-11 00:48:50 +0530

    Refactor TimeTableScreen to enhance UI and add class details

--- src/app/screens/timetable.tsx
+++ src/app/screens/timetable.tsx
@@ -1,3 +1,4 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import { useState } from 'react';
 import {
   Pressable,
@@ -8,12 +9,12 @@ import {
 } from 'react-native';
 
 const classes = [
-  { id: 1, subject: 'MANAGERIAL ECONOMICS', timeIn: '09:00 AM', timeOut: '10:00 AM' },
-  { id: 2, subject: 'MARKETING MANAGEMENT', timeIn: '10:00 AM', timeOut: '11:00 AM' },
-  { id: 3, subject: 'FINANCIAL ACCOUNTING', timeIn: '11:00 AM', timeOut: '11:00 AM' },
-  { id: 4, subject: 'ORGANIZATIONAL BEHAVIOUR', timeIn: '12:00 PM', timeOut: '01:00 PM' },
-  { id: 5, subject: 'DATA ANALYSIS IN EXCEL', timeIn: '01:00 PM', timeOut: '02:00 PM' },
-  { id: 6, subject: 'PYTHON FOR DATA ANALYTICS', timeIn: '02:00 PM', timeOut: '03:00 PM' },
+  { id: "MBB6102", subject: 'MANAGERIAL ECONOMICS', timeIn: '09:50', timeOut: '10:40', location: 'R3AB0211' },
+  { id: "MBA6109", subject: 'MARKETING MANAGEMENT', timeIn: '10:40', timeOut: '11:30', location: 'R3AB0211' },
+  { id: "MBA6113", subject: 'FINANCIAL ACCOUNTING', timeIn: '11:30', timeOut: '12:20', location: 'R3AB0211' },
+  { id: "MBA6115", subject: 'ORGANIZATIONAL BEHAVIOUR', timeIn: '12:20', timeOut: '01:10', location: 'R3AB0211' },
+  { id: "MBB6108", subject: 'DATA ANALYSIS IN EXCEL', timeIn: '01:10', timeOut: '02:00', location: 'R3AB0211' },
+  { id: "MBB6107", subject: 'PYTHON FOR DATA ANALYTICS', timeIn: '02:00', timeOut: '02:50', location: 'R3AB0211' },
 ];
 
 const weekDays = [
@@ -61,44 +62,80 @@ export default function TimeTableScreen() {
   return (
     <View style={styles.container}>
 
+      <View style={{marginTop: 10, marginBottom: 16}}>
       {/* Week days */}
-      <ScrollView
-        horizontal
-        style={styles.scrollView}
-        showsHorizontalScrollIndicator={false}
-      >
-        <View style={styles.dayContainer}>
+        <ScrollView horizontal={true} >
+          <View style={styles.dayContainer}>
 
-          {/* Week day filters */}
-          {weekDays.map(({ value, label }) => {
-            const isSelected = dayFilter === value;
-            const isToday = today === value;
+            {/* Week day filters */}
+            {weekDays.map(({ value, label }) => {
+              const isSelected = dayFilter === value;
+              const isToday = today === value;
 
-            return (
-              <Pressable
-                key={value}
-                onPress={() => setDayFilter(value)}
-                style={[
-                  styles.filter,
-                  isSelected && styles.active,
-                  isToday && { backgroundColor: '#1c2d42' },
-                ]}
-              >
-                <Text
+              return (
+                <Pressable
+                  key={value}
+                  onPress={() => setDayFilter(value)}
                   style={[
-                    styles.filterText,
-                    isSelected && { color: '#5de2f4' },
+                    styles.filter,
+                    isSelected && styles.active,
                     isToday && { backgroundColor: '#1c2d42' },
                   ]}
                 >
-                  {label}
-                </Text>
-              </Pressable>
-            );
-          })}
-        </View>
-      </ScrollView>
+                  <Text
+                    style={[
+                      styles.filterText,
+                      isSelected && { color: '#5de2f4' },
+                      isToday && { backgroundColor: '#1c2d42' },
+                    ]}
+                  >
+                    {label}
+                  </Text>
+                </Pressable>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </View>
 
+      {/* Section Divider */}
+      <View style={styles.sectionDivider} />
+
+
+      {/* Classes */}
+      <View style={{ flex: 1}}>
+      <ScrollView>
+        {classes.map(({ id, subject, timeIn, timeOut, location }) => (
+          <View key={id} style={styles.classContainer}>
+            {/* Time details */}
+            <View style={styles.timeContainer}>
+              <Text style={[styles.timeText, { fontSize: 18, color: '#5de2f4', fontWeight: '800' }]}>{timeIn}</Text>
+              <MaterialIcons name="arrow-downward" size={25} color="#9ca8bb" />
+              <Text style={[styles.timeText, { color: '#6f757d' }]}>{timeOut}</Text>
+            </View>
+
+            {/* Subject details */}
+            <View style={styles.detailContainer}>
+              {/* <View style={styles.subjectParameterContainer} > */}
+                {/* <MaterialIcons name="donut-large" size={16} color="#9ca8bb" style={{ marginRight: 4 }} /> */}
+                <Text style={[styles.idText]}>{id}</Text>
+              {/* </View> */}
+
+              {/* <View style={styles.subjectParameterContainer} > */}
+                {/* <MaterialIcons name="school" size={16} color="#9ca8bb" style={{ marginRight: 4 }} /> */}
+                <Text style={[styles.detailText, styles.subjectText]}>{subject}</Text>
+              {/* </View> */}
+              
+              <View style={styles.subjectParameterContainer} >
+                <MaterialIcons name="location-on" size={16} color="#9ca8bb" style={{ marginRight: 6 }} />
+                <Text style={styles.detailText}>{location}</Text>
+              </View>
+
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+      </View>
 
     </View>
   );
@@ -108,11 +145,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#080e18',
+    paddingHorizontal: 16,
   },
-
-  scrollView: {
-    marginTop: 10,
-    marginHorizontal: 10,
+  
+  sectionDivider: {
+    height: 1,
+    backgroundColor: '#263147',
+    marginBottom: 12,
   },
 
   dayContainer: {
@@ -138,5 +177,62 @@ const styles = StyleSheet.create({
   active: {
     borderWidth: 1,
     borderColor: '#08aabd',
+  },
+
+
+
+  classContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: '#121c2d',
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: '#233044',
+    padding: 16,
+    marginBottom: 12,
+  },
+  timeContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  detailContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 20,
+  },
+  subjectParameterContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexShrink: 1,
+  },
+
+
+
+  timeText: {
+    color: '#aeb8c8',
+    fontSize: 13,
+    fontWeight: '600',
+  },
+  detailText: {
+    color: '#575b61',
+    fontSize: 13,
+    fontWeight: '600',
+  },
+  idText: {
+    width: 80,
+    textAlign: 'center',
+    color: '#08aabd',
+    borderWidth: 1,
+    borderColor: '#055e68',
+    paddingHorizontal: 6,
+    paddingVertical: 2,
+    borderRadius: 50,
+    fontSize: 12,
+  },
+  subjectText: {
+    color: 'lightgray',
+    fontSize: 16,
+    fontWeight: '800',
+    marginVertical: 6,
   },
 });

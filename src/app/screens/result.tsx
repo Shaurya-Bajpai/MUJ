f28c502b1fefb4d336b2f680047ddf1da8c4431b
Shaurya Bajpai <shauryabajpai980@gmail.com>
2026-09-11 10:30:16 +0530

    Update ResultScreen to include totalCredits and GPA color coding

--- src/app/screens/result.tsx
+++ src/app/screens/result.tsx
@@ -8,15 +8,25 @@ export default function ResultScreen() {
         Branch: 'Computer Science & Engineering',
     }
     const result = [
-        { id: 1, CGPA: 9.5, GPA: 3.5, semester: 1 },
-        { id: 2, CGPA: 8.5, GPA: 3.0, semester: 2 },
-        { id: 3, CGPA: 7.9, GPA: 3.5, semester: 3 },
-        { id: 4, CGPA: 9.0, GPA: 3.2, semester: 4 },
+        { id: 1, CGPA: 9.5, GPA: 2.5, semester: 1, totalCredits: 20 },
+        { id: 2, CGPA: 8.5, GPA: 2.0, semester: 2, totalCredits: 22 },
+        { id: 3, CGPA: 7.9, GPA: 3.5, semester: 3, totalCredits: 12 },
+        { id: 4, CGPA: 9.0, GPA: 2.2, semester: 4, totalCredits: 20 },
     ]
     const highestGPA = Math.max(...result.map(item => item.GPA));
     const lowestGPA = Math.min(...result.map(item => item.GPA));
     const averageCGPA = result.reduce((sum, item) => sum + item.CGPA, 0) / result.length;
-    const totalCredits = result.reduce((sum, item) => sum + item.CGPA, 0);
+    const totalCredits = result.reduce((sum, item) => sum + item.totalCredits, 0);
+
+    const resultInfoHighestGpaColor = `${
+        highestGPA >= 3 ? '#19f3b9' : highestGPA >= 2 ? '#faa957' : '#FF4D65'
+    }`
+    const resultInfoLowestGpaColor = `${
+        lowestGPA >= 3 ? '#19f3b9' : lowestGPA >= 2 ? '#faa957' : '#FF4D65'
+    }`
+    const resultInfoAverageCgpaColor = `${
+        averageCGPA >= 3 ? '#19f3b9' : averageCGPA >= 2 ? '#faa957' : '#FF4D65'
+    }`
 
   return (
     <View style={styles.container}>
@@ -29,8 +39,8 @@ export default function ResultScreen() {
                 <Text style={styles.studentInfoText}>{studentInfo.Branch}</Text>
             </View>
             <View style={styles.currentGPAContainer} >
-                <Text style={[styles.studentInfoText, { color: '#5de2f4' }]}>Current GPA</Text>
-                <Text style={[styles.studentInfoText, { color: '#5de2f4' }]}>{result[result.length - 1].GPA}</Text>
+                <Text style={[styles.studentInfoText, { color: '#5de2f4', fontWeight: 'bold' }]}>Current GPA</Text>
+                <Text style={[styles.studentInfoText, { color: '#5de2f4', fontWeight: 'bold' }]}>{result[result.length - 1].GPA}</Text>
             </View>
         </View>
 
@@ -42,22 +52,22 @@ export default function ResultScreen() {
                     <View>
                         <View style={{flexDirection: 'row', gap: 12}}>
                             <View style={styles.resultItemContainer}>
-                                <Text style={[styles.resultHeadingText]}>Highest CGPA</Text>
-                                <Text style={[styles.resultText, { fontSize: 16, color: '#5de2f4', fontWeight: 'bold' }]}>{highestGPA}</Text>
+                                <Text style={[styles.resultHeadingText]}>Highest GPA</Text>
+                                <Text style={[styles.resultText, { color: resultInfoHighestGpaColor }]}>{highestGPA}</Text>
                             </View>
                             <View style={styles.resultItemContainer}>
-                                <Text style={[styles.resultHeadingText]}>Lowest CGPA</Text>
-                                <Text style={[styles.resultText, { fontSize: 16, color: '#5de2f4', fontWeight: 'bold' }]}>{lowestGPA}</Text>
+                                <Text style={[styles.resultHeadingText]}>Lowest GPA</Text>
+                                <Text style={[styles.resultText, { color: resultInfoLowestGpaColor }]}>{lowestGPA}</Text>
                             </View>
                         </View>
                         <View style={{flexDirection: 'row', gap: 12}}>
                             <View style={styles.resultItemContainer}>
                                 <Text style={[styles.resultHeadingText]}>Average CGPA</Text>
-                                <Text style={[styles.resultText, { fontSize: 16, color: '#5de2f4', fontWeight: 'bold' }]}>{averageCGPA}</Text>
+                                <Text style={[styles.resultText, { color: resultInfoAverageCgpaColor }]}>{averageCGPA}</Text>
                             </View>
                             <View style={styles.resultItemContainer}>
                                 <Text style={[styles.resultHeadingText]}>Total Credits</Text>
-                                <Text style={[styles.resultText, { fontSize: 16, color: '#5de2f4', fontWeight: 'bold' }]}>{totalCredits}</Text>
+                                <Text style={[styles.resultText, { color: '#5de2f4' }]}>{totalCredits}</Text>
                             </View>
                         </View>
                     </View>
@@ -110,12 +120,14 @@ const styles = StyleSheet.create({
   },
   resultText: {
     color: '#70747c',
-    fontSize: 14,
+    fontSize: 25,
     textAlign: 'center',
+    fontWeight: 'bold',
   },
   resultHeadingText: {
     color: '#70747c',
     fontSize: 12,
     marginBottom: 4,
+    fontWeight: '600',
   },
 })
